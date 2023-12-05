b7263dc08c390b7489764d9953874c1d3d38707d
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-12-05 20:12:20 +0100

    Create login page object model

---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    @FindBy(id = "login-username-or-email")
+    private WebElement usernameField;
+
+    @FindBy(id = "login-password")
+    private WebElement passwordField;
+
+    @FindBy(id = "login-submit-button")
+    private WebElement loginButton;
+    public LoginPage(WebDriver driver){
+        PageFactory.initElements(driver, this);}
+
+    public WebElement getUsernameField() {
+        return usernameField;
+    }
+
+    public WebElement getPasswordField() {
+        return passwordField;
+    }
+
+    public WebElement getLoginButton() {
+        return loginButton;
+    }
+}
