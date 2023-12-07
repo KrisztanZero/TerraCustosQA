a3ea11afed8593b13d9319826537a97539c6fcc3
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-12-07 01:16:43 +0100

    Add WebDriverWait to BaseTest

--- src/test/java/baseTests/BaseTest.java
+++ src/test/java/baseTests/BaseTest.java
@@ -10,16 +10,19 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.ExtentManager;
 import utilities.PropertyLoader;
 import utilities.WebDriverSetup;
 
+import java.time.Duration;
 import java.util.Properties;
 
 @TestInstance(Lifecycle.PER_CLASS)
 public class BaseTest {
 
     protected WebDriver driver;
+    protected WebDriverWait wait;
     protected ExtentReports extent;
     protected ExtentTest test;
     protected static final Logger logger = LogManager.getLogger(BaseTest.class);
@@ -32,9 +35,11 @@ public class BaseTest {
         test = ExtentManager.createTest(getClass().getSimpleName());
 
         driver = WebDriverSetup.getDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get(testProperties.getProperty("URL"));
 
         logger.info("Setting up the test");
+        test.log(Status.INFO, "Setting up the test");
     }
 
     @AfterAll
