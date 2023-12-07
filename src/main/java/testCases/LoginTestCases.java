2a829d9b08406471eeb8c8fe35dfa3f44460d080
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-12-07 01:20:48 +0100

    Complete LoginTestCases

--- src/main/java/testCases/LoginTestCases.java
+++ src/main/java/testCases/LoginTestCases.java
@@ -1,4 +1,19 @@
 package testCases;
 
+import actions.LoginActions;
+import org.openqa.selenium.WebDriver;
+
 public class LoginTestCases {
+
+    private LoginActions loginActions;
+
+    public LoginTestCases(WebDriver driver) {
+        this.loginActions = new LoginActions(driver);
+    }
+
+    public void performLogin(String usernameOrEmail, String password){
+        loginActions.enterUsername(usernameOrEmail);
+        loginActions.enterPassword(password);
+        loginActions.clickLoginButton();
+    }
 }
