2b0eca4e1663a3975f1e0421d103d7dce681f6db
KrisztanZero <s.molnar.krisztian@gmail.com>
2023-12-07 03:10:53 +0100

    Get welcome message

--- src/main/java/actions/NavbarActions.java
+++ src/main/java/actions/NavbarActions.java
@@ -2,18 +2,35 @@ package actions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import pages.Navbar;
 
 public class NavbarActions {
     private Navbar navbar;
+    private WebDriverWait wait;
 
-    public NavbarActions(WebDriver driver) {
+    public NavbarActions(WebDriver driver, WebDriverWait wait) {
         this.navbar = new Navbar(driver);
+        this.wait = wait;
     }
 
-    public void getProfileFromWelcomeMessage(){
-        WebElement welcomeMessage = navbar.getWelcomeMessage();
+    public String getWelcomeMessage(){
+        WebElement welcomeMessage = waitForElementToBeClickable(navbar.getWelcomeMessage());
+        return welcomeMessage.getText();
+    }
+
+    public void navigateProfileFromWelcomeMessage(){
+        WebElement welcomeMessage = waitForElementToBeClickable(navbar.getWelcomeMessage());
         welcomeMessage.click();
     }
 
+    public void clickOnProfileIcon(){
+        WebElement navbarProfileIcon = waitForElementToBeClickable(navbar.getProfileIcon());
+        navbarProfileIcon.click();
+    }
+
+    private WebElement waitForElementToBeClickable(WebElement element){
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
 }
